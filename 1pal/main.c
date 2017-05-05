cab47c9b239a7d7e7c168dc113910963c8a16718
Jacob Moen <jacmoe.dk@gmail.com>
2017-05-05 19:03:54 +0200

    Nasl lib update

--- 1pal/main.c
+++ 1pal/main.c
@@ -73,7 +73,7 @@ static int init()
     nasl_script_init();
     nasl_script_run("assets/scripts/init.bas");
 
-    nasl_graphics_init(800, 600, "nasl test", 0);
+    nasl_graphics_init(800, 600, "nasl test", 0, 1);
 
     return 1;
 }
