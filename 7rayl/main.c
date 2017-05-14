5b5ba221b33202bfa50503a577b143920e6f6d4a
Jacob Moen <jacmoe.dk@gmail.com>
2017-05-14 15:41:50 +0200

    Lightsourced raycast demo

---
+++ 7rayl/main.c
@@ -0,0 +1,388 @@
+/*
+*   This file is part of the
+* ███╗   ██╗███████╗ ██████╗  ██████╗  █████╗ ██████╗ ██████╗ ███████╗███╗   ██╗███████╗
+* ████╗  ██║██╔════╝██╔═══██╗██╔════╝ ██╔══██╗██╔══██╗██╔══██╗██╔════╝████╗  ██║██╔════╝
+* ██╔██╗ ██║█████╗  ██║   ██║██║  ███╗███████║██████╔╝██║  ██║█████╗  ██╔██╗ ██║███████╗
+* ██║╚██╗██║██╔══╝  ██║   ██║██║   ██║██╔══██║██╔══██╗██║  ██║██╔══╝  ██║╚██╗██║╚════██║
+* ██║ ╚████║███████╗╚██████╔╝╚██████╔╝██║  ██║██║  ██║██████╔╝███████╗██║ ╚████║███████║
+* ╚═╝  ╚═══╝╚══════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝
+*   project : https://github.com/jacmoe/neogardens
+*
+*   Copyright 2017 Jacob Moen
+*
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+
+#include "nasl_graphics.h"
+#include "nasl_buffer.h"
+#include "nasl_image.h"
+#include "nasl_draw.h"
+#include "nasl_sprite.h"
+
+#define mapWidth 24
+#define mapHeight 24
+
+int worldMap[mapWidth][mapHeight] =
+    {
+        {8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 4, 4, 6, 4, 4, 6, 4, 6, 4, 4, 4, 6, 4},
+        {8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
+        {8, 0, 3, 3, 0, 0, 0, 0, 0, 8, 8, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6},
+        {8, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6},
+        {8, 0, 3, 3, 0, 0, 0, 0, 0, 8, 8, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
+        {8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 4, 0, 0, 0, 0, 0, 6, 6, 6, 0, 6, 4, 6},
+        {8, 8, 8, 8, 0, 8, 8, 8, 8, 8, 8, 4, 4, 4, 4, 4, 4, 6, 0, 0, 0, 0, 0, 6},
+        {7, 7, 7, 7, 0, 7, 7, 7, 7, 0, 8, 0, 8, 0, 8, 0, 8, 4, 0, 4, 0, 6, 0, 6},
+        {7, 7, 0, 0, 0, 0, 0, 0, 7, 8, 0, 8, 0, 8, 0, 8, 8, 6, 0, 0, 0, 0, 0, 6},
+        {7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 6, 0, 0, 0, 0, 0, 4},
+        {7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 6, 0, 6, 0, 6, 0, 6},
+        {7, 7, 0, 0, 0, 0, 0, 0, 7, 8, 0, 8, 0, 8, 0, 8, 8, 6, 4, 6, 0, 6, 6, 6},
+        {7, 7, 7, 7, 0, 7, 7, 7, 7, 8, 8, 4, 0, 6, 8, 4, 8, 3, 3, 3, 0, 3, 3, 3},
+        {2, 2, 2, 2, 0, 2, 2, 2, 2, 4, 6, 4, 0, 0, 6, 0, 6, 3, 0, 0, 0, 0, 0, 3},
+        {2, 2, 0, 0, 0, 0, 0, 2, 2, 4, 0, 0, 0, 0, 0, 0, 4, 3, 0, 0, 0, 0, 0, 3},
+        {2, 0, 0, 0, 0, 0, 0, 0, 2, 4, 0, 0, 0, 0, 0, 0, 4, 3, 0, 0, 0, 0, 0, 3},
+        {1, 0, 0, 0, 0, 0, 0, 0, 1, 4, 4, 4, 4, 4, 6, 0, 6, 3, 3, 0, 0, 0, 3, 3},
+        {2, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 1, 2, 2, 2, 6, 6, 0, 0, 5, 0, 5, 0, 5},
+        {2, 2, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 2, 2, 0, 5, 0, 5, 0, 0, 0, 5, 5},
+        {2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 5, 0, 5, 0, 5, 0, 5, 0, 5},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5},
+        {2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 5, 0, 5, 0, 5, 0, 5, 0, 5},
+        {2, 2, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 2, 2, 0, 5, 0, 5, 0, 0, 0, 5, 5},
+        {2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5}};
+
+double posX = 22.0, posY = 11.5;    //x and y start position
+double dirX = -1.0, dirY = 0.0;     //initial direction vector
+double planeX = 0.0, planeY = 0.66; //the 2d raycaster version of camera plane
+
+double time = 0;    //time of current frame
+double oldTime = 0; //time of previous frame
+double frameTime = 0;
+
+static int init(int width, int height);
+static int shutdown();
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
+
+void draw_maze(Buffer *buffer, SpriteSheet textures);
+
+int main()
+{
+    int buffer_width = 320;
+    int buffer_height = 240;
+
+    init(buffer_width, buffer_height);
+
+    glfwSwapInterval(1);
+
+    // Create main buffer
+    Buffer *buffer = nasl_buffer_create(buffer_width, buffer_height);
+    nasl_buffer_set_mainbuffer(buffer);
+
+    // Load textures
+    SpriteSheet textures = nasl_sprite_load("assets/textures/sjswalls2.bmp", 4, 3);
+
+    // Main loop
+    while (nasl_graphics_running())
+    {
+        oldTime = time;
+        time = glfwGetTime();
+        frameTime = time - oldTime; //frameTime is the time this frame has taken, in seconds
+
+        // Event polling
+        nasl_graphics_poll_events();
+
+        // draw box and maze
+        nasl_buffer_clear(buffer, GREY1);
+        draw_maze(buffer, textures);
+        //draw_box(buffer);
+
+        // Render the main buffer
+        nasl_graphics_render(buffer);
+        // Swap buffers
+        nasl_graphics_present();
+    }
+
+    nasl_sprite_delete(textures);
+    // Destroy the main buffer
+    nasl_buffer_destroy(buffer);
+
+    shutdown();
+}
+
+static Buffer *get_texture(SpriteSheet textures, int idx)
+{
+    int col = idx % textures.cols;
+    int row = idx / textures.cols;
+    return nasl_sprite_get(textures, row, col);
+}
+
+static uint32_t nasl_color_divide(uint32_t c, double intensity)
+{
+    uint8_t red = GETR(c);
+    uint8_t green = GETG(c);
+    uint8_t blue = GETB(c);
+
+    red /= intensity;
+    green /= intensity;
+    blue /= intensity;
+
+    return BUILDRGB(red, green, blue);
+}
+
+
+void draw_maze(Buffer *buffer, SpriteSheet textures)
+{
+    for (int x = 0; x < buffer->width; x++)
+    {
+        //calculate ray position and direction
+        double cameraX = 2 * x / (double)buffer->width - 1; //x-coordinate in camera space
+        double rayPosX = posX;
+        double rayPosY = posY;
+        double rayDirX = dirX + planeX * cameraX;
+        double rayDirY = dirY + planeY * cameraX;
+
+        //which box of the map we're in
+        int mapX = (int)rayPosX;
+        int mapY = (int)rayPosY;
+
+        //length of ray from current position to next x or y-side
+        double sideDistX;
+        double sideDistY;
+
+        //length of ray from one x or y-side to next x or y-side
+        double deltaDistX = sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
+        double deltaDistY = sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
+        double perpWallDist;
+
+        //what direction to step in x or y-direction (either +1 or -1)
+        int stepX;
+        int stepY;
+
+        int hit = 0; //was there a wall hit?
+        int side;    //was a NS or a EW wall hit?
+
+        //calculate step and initial sideDist
+        if (rayDirX < 0)
+        {
+            stepX = -1;
+            sideDistX = (rayPosX - mapX) * deltaDistX;
+        }
+        else
+        {
+            stepX = 1;
+            sideDistX = (mapX + 1.0 - rayPosX) * deltaDistX;
+        }
+        if (rayDirY < 0)
+        {
+            stepY = -1;
+            sideDistY = (rayPosY - mapY) * deltaDistY;
+        }
+        else
+        {
+            stepY = 1;
+            sideDistY = (mapY + 1.0 - rayPosY) * deltaDistY;
+        }
+        //perform DDA
+        while (hit == 0)
+        {
+            //jump to next map square, OR in x-direction, OR in y-direction
+            if (sideDistX < sideDistY)
+            {
+                sideDistX += deltaDistX;
+                mapX += stepX;
+                side = 0;
+            }
+            else
+            {
+                sideDistY += deltaDistY;
+                mapY += stepY;
+                side = 1;
+            }
+            //Check if ray has hit a wall
+            if (worldMap[mapX][mapY] > 0)
+                hit = 1;
+        }
+
+        //Calculate distance of perpendicular ray (oblique distance will give fisheye effect!)
+        if (side == 0)
+            perpWallDist = (mapX - rayPosX + (1 - stepX) / 2) / rayDirX;
+        else
+            perpWallDist = (mapY - rayPosY + (1 - stepY) / 2) / rayDirY;
+
+        //Calculate height of line to draw on screen
+        int lineHeight = (int)(buffer->height / perpWallDist);
+
+        //calculate lowest and highest pixel to fill in current stripe
+        int drawStart = -lineHeight / 2 + buffer->height / 2;
+        if (drawStart < 0)
+            drawStart = 0;
+        int drawEnd = lineHeight / 2 + buffer->height / 2;
+        if (drawEnd >= buffer->height)
+            drawEnd = buffer->height - 1;
+        //texturing calculations
+        int texNum = worldMap[mapX][mapY] - 1; //1 subtracted from it so that texture 0 can be used!
+        Buffer* texture = get_texture(textures, texNum);
+
+        //calculate value of wallX
+        double wallX; //where exactly the wall was hit
+        if (side == 0)
+            wallX = rayPosY + perpWallDist * rayDirY;
+        else
+            wallX = rayPosX + perpWallDist * rayDirX;
+        wallX -= floor((wallX));
+
+        //x coordinate on the texture
+        int texX = (int)(wallX * (double)textures.width);
+        if (side == 0 && rayDirX > 0)
+            texX = textures.width - texX - 1;
+        if (side == 1 && rayDirY < 0)
+            texX = textures.width - texX - 1;
+        for (int y = drawStart; y < drawEnd; y++)
+        {
+            int d = y * 256 - buffer->height * 128 + lineHeight * 128; //256 and 128 factors to avoid floats
+            int texY = ((d * textures.height) / lineHeight) / 256;
+            uint32_t color = nasl_buffer_get_pixel(texture, texX, texY);
+            //make color darker for y-sides: R, G and B byte each divided through two with a "shift" and an "and"
+            if (side == 1)
+                color = (color >> 1) & 8355711;
+                
+            if(perpWallDist > 1)
+                color = nasl_color_divide(color, perpWallDist);
+            nasl_buffer_set_pixel(buffer, x, y, color);
+        }
+
+        //FLOOR CASTING
+        double floorXWall, floorYWall; //x, y position of the floor texel at the bottom of the wall
+
+        //4 different wall directions possible
+        if (side == 0 && rayDirX > 0)
+        {
+            floorXWall = mapX;
+            floorYWall = mapY + wallX;
+        }
+        else if (side == 0 && rayDirX < 0)
+        {
+            floorXWall = mapX + 1.0;
+            floorYWall = mapY + wallX;
+        }
+        else if (side == 1 && rayDirY > 0)
+        {
+            floorXWall = mapX + wallX;
+            floorYWall = mapY;
+        }
+        else
+        {
+            floorXWall = mapX + wallX;
+            floorYWall = mapY + 1.0;
+        }
+
+        double distWall, distPlayer, currentDist;
+
+        distWall = perpWallDist;
+        distPlayer = 0.0;
+
+        if (drawEnd < 0)
+            drawEnd = buffer->height; //becomes < 0 when the integer overflows
+
+        //draw the floor from drawEnd to the bottom of the screen
+        for (int y = drawEnd; y < buffer->height; y++)
+        {
+            currentDist = buffer->height / (2.0 * y - buffer->height); //you could make a small lookup table for this instead
+
+            double weight = (currentDist - distPlayer) / (distWall - distPlayer);
+
+            double currentFloorX = weight * floorXWall + (1.0 - weight) * posX;
+            double currentFloorY = weight * floorYWall + (1.0 - weight) * posY;
+
+            int floorTexX, floorTexY;
+            floorTexX = (int)(currentFloorX * textures.width) % textures.width;
+            floorTexY = (int)(currentFloorY * textures.height) % textures.height;
+
+            //floor
+            texture = get_texture(textures, 5);
+            uint32_t color = (texture->pixels[textures.width * floorTexY + floorTexX] >> 1) & 8355711;
+            color = (color >> 1) & 8355711;
+            nasl_buffer_set_pixel(buffer, x, y, color);
+            //ceiling (symmetrical!)
+            texture = get_texture(textures, 10);
+            color = texture->pixels[textures.width * floorTexY + floorTexX];
+            color = (color >> 1) & 8355711;
+            nasl_buffer_set_pixel(buffer, x, buffer->height - y, color);
+        }
+    }
+}
+
+static int init(int width, int height)
+{
+    nasl_graphics_init(width, height, "Neogardens Lightsourced Raycast Maze Demo", 0, 3);
+
+    glfwSetKeyCallback(nasl_graphics_get_window(), key_callback);
+
+    return 1;
+}
+
+static void handle_keypress(int key, int action)
+{
+    //speed modifiers
+    double moveSpeed = frameTime * 10.0; //the constant value is in squares/second
+    double rotSpeed = frameTime * 3.0;   //the constant value is in radians/second
+
+    // Do we want to move forward?
+    if (key == GLFW_KEY_W)
+    {
+        if (!worldMap[(int)(posX + dirX * moveSpeed)][(int)(posY)])
+            posX += dirX * moveSpeed;
+        if (!worldMap[(int)(posX)][(int)(posY + dirY * moveSpeed)])
+            posY += dirY * moveSpeed;
+    }
+    // or do we want to go backward?
+    else if (key == GLFW_KEY_S)
+    {
+        if (!worldMap[(int)(posX - dirX * moveSpeed)][(int)(posY)])
+            posX -= dirX * moveSpeed;
+        if (!worldMap[(int)(posX)][(int)(posY - dirY * moveSpeed)])
+            posY -= dirY * moveSpeed;
+    }
+    // Do we want to turn left?
+    if (key == GLFW_KEY_A)
+    {
+        //both camera direction and camera plane must be rotated
+        double oldDirX = dirX;
+        dirX = dirX * cos(rotSpeed) - dirY * sin(rotSpeed);
+        dirY = oldDirX * sin(rotSpeed) + dirY * cos(rotSpeed);
+        double oldPlaneX = planeX;
+        planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
+        planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
+    }
+    // or do we want to turn right?
+    else if (key == GLFW_KEY_D)
+    {
+        //both camera direction and camera plane must be rotated
+        double oldDirX = dirX;
+        dirX = dirX * cos(-rotSpeed) - dirY * sin(-rotSpeed);
+        dirY = oldDirX * sin(-rotSpeed) + dirY * cos(-rotSpeed);
+        double oldPlaneX = planeX;
+        planeX = planeX * cos(-rotSpeed) - planeY * sin(-rotSpeed);
+        planeY = oldPlaneX * sin(-rotSpeed) + planeY * cos(-rotSpeed);
+    }
+}
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+    else
+    {
+        handle_keypress(key, action);
+    }
+}
+
+static int shutdown()
+{
+    nasl_graphics_shutdown();
+
+    return 1;
+}
