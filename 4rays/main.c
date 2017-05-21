6be3fb81c5f011fb9e9a0ec381a5c0871f966600
Jacob Moen <jacmoe.dk@gmail.com>
2017-05-21 15:40:44 +0200

    Progress after SDL2 attempts

--- 4rays/main.c
+++ 4rays/main.c
@@ -301,7 +301,7 @@ static void handle_keypress(int key, int action)
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    if ((key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) || (key == GLFW_KEY_Q && action == GLFW_PRESS))
     {
         glfwSetWindowShouldClose(window, GLFW_TRUE);
     } else {
